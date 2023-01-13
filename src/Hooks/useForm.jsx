4ec63e08f51477ac295aa991c72bf99aa3867793
Leandro <thivithale@hotmail.com>
2023-01-12 23:03:05 -0300

    Integrando a validação de login

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const types = {
   email: {
     regex:
-      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/,
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
 
     message: 'Preencha um email válido',
   },
@@ -11,16 +11,24 @@ const types = {
 
 const useForm = (type) => {
   const [value, setValue] = React.useState('');
-  const [error, setError] = React.useState('');
+  const [error, setError] = React.useState(null);
 
   function validate(value) {
     if (type === false) return true;
     if (value.length === 0) {
       setError('Preencha um valor');
+      return false;
+    } else if (types[type] && types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
     }
   }
 
   function onChange({ target }) {
+    if (error) validate(target.value);
     setValue(target.value);
   }
 
@@ -28,6 +36,9 @@ const useForm = (type) => {
     value,
     setValue,
     onChange,
+    error,
+    validate: () => validate(value),
+    onBLur: () => validate(value),
   };
 };
 
